c840a578a29b296986a3e74df6753f5c4ce1c3ec
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2019-12-13 12:34:43 +0100

    Change index to return a render_template method instead of the message string

--- friends_app/__init__.py
+++ friends_app/__init__.py
@@ -1,12 +1,14 @@
 import random
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
+
 
 
 app = Flask(__name__)
 api = Api(app)
-
+# this allows the css file to refresh instead of being cached and not resembling changes
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # fmt: off
 from friends_app import db  # circular import case
@@ -22,8 +24,7 @@ def index():
         f"Your episode for the current moment is {info['title']} "
         f"(season: {info['season']}, episode: {info['episode']})"
     )
-    return message
-
+    return render_template('index.html', message=message)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=False, threaded=True)
