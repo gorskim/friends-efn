7c6faf740b002422bb9674505e9293b6242c3728
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2019-12-14 16:16:37 +0100

    Add method to serve a favicon.

--- friends_app/__init__.py
+++ friends_app/__init__.py
@@ -1,6 +1,7 @@
 import random
+import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_restful import Api
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 from friends_app import db  # circular import case
 # fmt: on
 
+
 @app.route("/")
 def index():
     episodes = db.get_db()
@@ -22,6 +24,13 @@ def index():
         f"(season: {info['season']}, episode: {info['episode']})"
     )
     return render_template('index.html', message=message)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=False, threaded=True)
