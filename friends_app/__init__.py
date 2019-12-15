9b05dba9ce5957349ecbf70529e1119488831f82
Mateusz Gorski <mateu.gorski@gmail.com>
2019-12-15 21:00:58 +0100

    Apply black linter

--- friends_app/__init__.py
+++ friends_app/__init__.py
@@ -7,7 +7,7 @@ from flask_restful import Api
 app = Flask(__name__)
 api = Api(app)
 # this allows the css file to refresh instead of being cached and not resembling changes
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 # fmt: off
 from friends_app import db  # circular import case
@@ -21,14 +21,18 @@ def index():
     info = episodes[chosen]
     title = f"{info['title']}"
     season_episode = f"(season: {info['season']}, episode: {info['episode']})"
-    return render_template('index.html', title=title,
-                           season_episode=season_episode)
+    return render_template(
+        "index.html", title=title, season_episode=season_episode
+    )
 
 
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
 
 if __name__ == "__main__":
