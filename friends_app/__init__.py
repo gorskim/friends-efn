c05edc1f27ed3e4c5ee912519b24c61a9284ec28
Mateusz <45688359+mateuszgrzybek@users.noreply.github.com>
2019-12-15 21:02:53 +0100

    Fix for compatibility and adjustments
    * Change url for the main bg image to match Flasks wishes. Hope for the best
    
    * Change the background image and overlay to both be html elements.
    
    * Change transition to be compatible with all browsers. Fix overlay opacity and change display of elements to behave properly in all browsers.
    
    * Add method to serve a favicon.
    
    * Add favicon.
    
    * Remove no more needed css rules.
    
    * Refactor message to be displayed as 3 separate paragraphs for improved readability.
    
    * Apply black linter

--- friends_app/__init__.py
+++ friends_app/__init__.py
@@ -1,27 +1,39 @@
 import random
+import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_restful import Api
 
 app = Flask(__name__)
 api = Api(app)
 # this allows the css file to refresh instead of being cached and not resembling changes
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 # fmt: off
 from friends_app import db  # circular import case
 # fmt: on
+
 
 @app.route("/")
 def index():
     episodes = db.get_db()
     chosen = random.choice(list(episodes.keys()))
     info = episodes[chosen]
-    message = (
-        f"Your episode for the current moment is {info['title']} "
-        f"(season: {info['season']}, episode: {info['episode']})"
+    title = f"{info['title']}"
+    season_episode = f"(season: {info['season']}, episode: {info['episode']})"
+    return render_template(
+        "index.html", title=title, season_episode=season_episode
     )
-    return render_template('index.html', message=message)
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=False, threaded=True)
