5037a90ddc0337033cdcd0cba5b3c20186c09528
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2019-12-21 18:15:59 +0100

    Change details display to fade-in/fade-out animation. Add some fancy hover effects at the switch button.

--- friends_app/static/js/coolScript.js
+++ friends_app/static/js/coolScript.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
-    $(".details-content").each(function() {
-        $(this).css("height", $(this).height()+"px");
-        $(this).hide();
-    })
     $('.hidden').animate({'opacity': 1}, 3000);
+    $('.btn-switch').animate({'opacity': 1}, 3000);
 });
 
 getNewEpisode = () => {
     window.location.reload()
 }
 
-showOrHideDetails = () => {
-    var details = $(".details-content");
-    details.slideToggle(400);
-    details.css("display", "flex");
-    $("#friends-logo-image").css("width", "500px");
+changeDisplay = () => {
+    if ($('.hidden').css("opacity") == 1) {
+        showDetails();
+    }
+    else if ($('.details').css("opacity") == 1) {
+        showMessage();
+    }
+}
+
+showDetails = () => {
+    $('.hidden').animate({'opacity': 0}, 1000);
+    $(".details").animate({'opacity': 1}, 1000);
+    document.getElementsByClassName("btn-switch")[0].innerHTML = "Take me back!";
+}
+
+showMessage = () => {
+    $(".details").animate({'opacity': 0}, 1000);
+    $('.hidden').animate({'opacity': 1}, 1000);
+    document.getElementsByClassName("btn-switch")[0].innerHTML = "Tell me more!";
 }
