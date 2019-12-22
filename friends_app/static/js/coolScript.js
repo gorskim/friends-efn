383ecf0159301328e9fb8f6b5536994783da2dbe
Mateusz <45688359+mateuszgrzybek@users.noreply.github.com>
2019-12-22 12:21:15 +0100

    View switch (#5)
    * Create basis for displaying  episode description
    
    * Change details display to fade-in/fade-out animation. Add some fancy hover effects at the switch button.

--- friends_app/static/js/coolScript.js
+++ friends_app/static/js/coolScript.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
-    $('.hidden').animate({'opacity': 1}, 3000)
+    $('.hidden').animate({'opacity': 1}, 3000);
+    $('.btn-switch').animate({'opacity': 1}, 3000);
 });
 
 getNewEpisode = () => {
     window.location.reload()
+}
+
+changeDisplay = () => {
+    if ($('.hidden').css("opacity") == 1) {
+        showDetails();
+    }
+    else if ($('.details').css("opacity") == 1) {
+        showMessage();
+    }
+}
+
+showDetails = () => {
+    $('.hidden').animate({'opacity': 0}, 1000);
+    $(".details").animate({'opacity': 1}, 1000);
+    document.getElementsByClassName("btn-switch")[0].innerHTML = "Take me back!";
+}
+
+showMessage = () => {
+    $(".details").animate({'opacity': 0}, 1000);
+    $('.hidden').animate({'opacity': 1}, 1000);
+    document.getElementsByClassName("btn-switch")[0].innerHTML = "Tell me more!";
 }
