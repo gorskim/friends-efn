8a35941370e2b92dcc6494f31d9551a6b7355fb0
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2019-12-23 08:54:52 +0100

    Refactor js code

--- friends_app/static/js/coolScript.js
+++ friends_app/static/js/coolScript.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
-    $('.hidden').animate({'opacity': 1}, 3000);
-    $('.btn-switch').animate({'opacity': 1}, 3000);
-});
+var switchText;
+
+animateOpacity = (selector, value, time) => {
+    $(selector).animate({'opacity': value}, time);
+}
 
 getNewEpisode = () => {
     window.location.reload()
-}
+};
+
+$(document).ready(function() {
+    animateOpacity('.hidden', 1, 3000);
+    animateOpacity('.btn-switch', 1, 3000);
+    switchText = document.getElementsByClassName("btn-switch")[0].innerHTML;
+});
 
 changeDisplay = () => {
     if ($('.hidden').css("opacity") == 1) {
@@ -14,16 +21,16 @@ changeDisplay = () => {
     else if ($('.details').css("opacity") == 1) {
         showMessage();
     }
-}
+};
 
 showDetails = () => {
-    $('.hidden').animate({'opacity': 0}, 1000);
-    $(".details").animate({'opacity': 1}, 1000);
-    document.getElementsByClassName("btn-switch")[0].innerHTML = "Take me back!";
-}
+    animateOpacity('.hidden', 0, 1000);
+    animateOpacity('.details', 1, 1000);
+    switchText = "Take me back!";
+};
 
 showMessage = () => {
-    $(".details").animate({'opacity': 0}, 1000);
-    $('.hidden').animate({'opacity': 1}, 1000);
-    document.getElementsByClassName("btn-switch")[0].innerHTML = "Tell me more!";
-}
+    animateOpacity('.details', 0, 1000);
+    animateOpacity('.hidden', 1, 1000);
+    switchText = "Tell me more!";
+};
