b1793d0238373be8570e933eab95c982c3873306
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2020-01-02 18:40:42 +0100

    Add timer to track whether the user is idle. If so - fire up some pulsating effects!

--- friends_app/static/js/coolScript.js
+++ friends_app/static/js/coolScript.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
+    idleTime();
     $('.hidden').animate({'opacity': 1}, 3000);
     $('.btn-switch').animate({'opacity': 1}, 3000);
 });
 
 getNewEpisode = () => {
-    window.location.reload()
+    window.location.reload();
 }
 
 changeDisplay = () => {
@@ -26,4 +27,22 @@ showMessage = () => {
     $(".details").animate({'opacity': 0}, 1000);
     $('.hidden').animate({'opacity': 1}, 1000);
     document.getElementsByClassName("btn-switch")[0].innerHTML = "Tell me more!";
+}
+
+var idleTime = function() {
+    var time;
+    window.onload = resetIdleTime;
+    document.onmousemove = resetIdleTime;
+    document.onkeypress = resetIdleTime;
+
+    function pulsateLogo() {
+        $('#friends-logo-image').addClass('pulse')
+        }
+
+    function resetIdleTime() {
+        clearTimeout(time);
+        $('#friends-logo-image').removeClass('pulse')
+        console.log('reset')
+        time = setTimeout(pulsateLogo, 30000)
+    }
 }
