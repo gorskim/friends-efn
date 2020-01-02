854c0659984ac8dad53210bb679af2b675f6f8a3
Mateusz <45688359+mateuszgrzybek@users.noreply.github.com>
2020-01-02 20:51:38 +0100

    Fe/frontend idle timer (#7)
    * Refactor js code
    
    * Add timer to track whether the user is idle. If so - fire up some pulsating effects!
    
    * Remove console logs

--- friends_app/static/js/coolScript.js
+++ friends_app/static/js/coolScript.js
@@ -1,11 +1,19 @@
+var switchText;
+
 $(document).ready(function() {
+    idleTime();
     $('.hidden').animate({'opacity': 1}, 3000);
     $('.btn-switch').animate({'opacity': 1}, 3000);
+    switchText = document.getElementsByClassName("btn-switch")[0].innerHTML
 });
+
+animateOpacity = (selector, value, time) => {
+    $(selector).animate({'opacity': value}, time);
+}
 
 getNewEpisode = () => {
     window.location.reload()
-}
+};
 
 changeDisplay = () => {
     if ($('.hidden').css("opacity") == 1) {
@@ -14,16 +22,33 @@ changeDisplay = () => {
     else if ($('.details').css("opacity") == 1) {
         showMessage();
     }
-}
+};
 
 showDetails = () => {
-    $('.hidden').animate({'opacity': 0}, 1000);
-    $(".details").animate({'opacity': 1}, 1000);
-    document.getElementsByClassName("btn-switch")[0].innerHTML = "Take me back!";
-}
+    animateOpacity('.hidden', 0, 1000);
+    animateOpacity('.details', 1, 1000);
+    switchText = "Take me back!";
+};
 
 showMessage = () => {
-    $(".details").animate({'opacity': 0}, 1000);
-    $('.hidden').animate({'opacity': 1}, 1000);
-    document.getElementsByClassName("btn-switch")[0].innerHTML = "Tell me more!";
+    animateOpacity('.details', 0, 1000);
+    animateOpacity('.hidden', 1, 1000);
+    switchText = "Tell me more!";
+}
+
+var idleTime = function() {
+    var time;
+    window.onload = resetIdleTime;
+    document.onmousemove = resetIdleTime;
+    document.onkeypress = resetIdleTime;
+
+    function pulsateLogo() {
+        $('#friends-logo-image').addClass('pulse')
+        }
+
+    function resetIdleTime() {
+        clearTimeout(time);
+        $('#friends-logo-image').removeClass('pulse')
+        time = setTimeout(pulsateLogo, 30000)
+    }
 }
